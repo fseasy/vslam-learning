2d4c98c75f14d78756c8b4812ead439b30fce5f6
fseasy <readonlyfile@hotmail.com>
2022-08-04 13:34:48 +0800

    add opencv dep in cmake

--- visual_odometry/orb/intensity_centroid.cpp
+++ visual_odometry/orb/intensity_centroid.cpp
@@ -1,12 +1,16 @@
-#include <opencv/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
-void CalcIntensityCentroid(const cv::Mat& img， 
-    const cv::KeyPoint& patch_center, 
+void CalcIntensityCentroid(const cv::Mat& img,
+    const cv::Point2f& patch_center, 
     std::size_t patch_size) {
     std::size_t m00 = 0U;
     std::size_t m01 = 0U;
     std::size_t m10 = 0U;
     int patch_half_size = static_cast<int>(patch_size / 2);
-    
+    // con
 
+}
+
+int main() {
+    return 0;
 }
