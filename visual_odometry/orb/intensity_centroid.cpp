867a52cfdcc4ea325f2a28de7e8aaac9aea1eb43
fseasy <readonlyfile@hotmail.com>
2022-08-05 13:34:02 +0800

    it can't draw text? need to check

--- visual_odometry/orb/intensity_centroid.cpp
+++ visual_odometry/orb/intensity_centroid.cpp
@@ -4,11 +4,11 @@
 
 bool CalcIntensityCentroid(const cv::Mat& img,
     const cv::Point2f& patch_center, 
-    std::size_t patch_size,
+    int patch_size,
     cv::Point2f& centroid_point) {
     // corner check
     int patch_half_size = static_cast<int>(patch_size / 2);
-    // ---c-- (patch_size is odd, )
+    //  patch_size is odd, and center in right, like: ---|c--
     if (patch_center.x - patch_half_size < 0
         || patch_center.y - patch_half_size < 0
         || patch_center.x + patch_half_size - 1 >= img.cols
@@ -36,6 +36,10 @@ bool CalcIntensityCentroid(const cv::Mat& img,
     return true;
 }
 
+void drawIntensityCentroid(const cv::Mat& img, 
+    const cv::Point2f& center, 
+    const cv::Point2f& centroid);
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "usage: " << argv[0] << " img-path" << std::endl;
@@ -50,10 +54,40 @@ int main(int argc, char* argv[]) {
     }
     cv::imshow("input-image-show", img);
     cv::waitKey(5000);
-    cv::Point2f center_point(30.f, 30.f);
+    int width = img.cols;
+    int height = img.rows;
+    cv::Point2f center_point(width / 2, height / 2);
+    int patch_size = std::min(width, height);
     cv::Point2f centroid_point{};
-    bool is_ok = CalcIntensityCentroid(img, center_point, 16U, centroid_point);
-    std::cerr << "calc intensity centroid " << std::boolalpha << is_ok << std::endl;
-    std::cerr << "centroid = " << centroid_point << std::endl;
+    bool is_ok = CalcIntensityCentroid(img, center_point, patch_size, centroid_point);
+    if (!is_ok) {
+        std::cerr << "Calc intensity centroid failed." << std::endl;
+        return -1;
+    }
+    std::cerr << "Intensity centroid = " << centroid_point << std::endl;
+    drawIntensityCentroid(img, center_point, centroid_point);
     return 0;
+}
+
+void drawIntensityCentroid(const cv::Mat& img, 
+    const cv::Point2f& center, 
+    const cv::Point2f& centroid) {
+    cv::Mat img_draw{};
+    cv::cvtColor(img, img_draw, cv::COLOR_GRAY2BGR);
+    
+    // coordinates
+    cv::Point2f y_axis_start_pnt(img.cols / 2, img.rows * 0.1f);
+    cv::Point2f y_axis_end_pnt(img.cols / 2, img.rows * 0.9f);
+    cv::Point2f x_axis_start_pnt(img.cols * 0.1f, img.rows / 2);
+    cv::Point2f x_axis_end_pnt(img.cols * 0.9f, img.rows / 2);
+    // cv::arrowedLine(img_draw, y_axis_start_pnt, y_axis_end_pnt, CV_RGB(255, 0, 0), 
+    //     1, cv::LINE_AA, 0, 0.1);
+    // cv::arrowedLine(img_draw, x_axis_start_pnt, x_axis_end_pnt, CV_RGB(255, 0, 0),
+    //     1, cv::LINE_AA, 0, 0.1);
+    cv::Point2f y_axis_tag_pnt = y_axis_end_pnt + cv::Point2f(10, -10);
+    cv::putText(img_draw, "yyyyyyy", y_axis_end_pnt, cv::FONT_HERSHEY_SIMPLEX, 20, CV_RGB(0, 0, 0));
+    cv::Point2f x_axis_tag_pnt = x_axis_end_pnt + cv::Point2f(-10, 10);
+    cv::putText(img_draw, "xzzz", x_axis_tag_pnt, cv::FONT_HERSHEY_SIMPLEX, 20, CV_RGB(0, 0, 0));
+    cv::imshow("centroid-image", img_draw);
+    cv::waitKey(3000);
 }
