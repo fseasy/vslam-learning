0857114860b39ca583ac104a91572dba4cf678cc
fseasy <readonlyfile@hotmail.com>
2022-08-07 11:08:54 +0800

    update

--- visual_odometry/orb/intensity_centroid.cpp
+++ visual_odometry/orb/intensity_centroid.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #include <opencv2/opencv.hpp>
 
@@ -29,8 +30,8 @@ bool CalcIntensityCentroid(const cv::Mat& img,
             m10 += dx * intensity;
         }
     }
-    float patch_centroid_x = static_cast<float>(m10) / m00;
-    float patch_centroid_y = static_cast<float>(m01) / m00;
+    float patch_centroid_x = m10 / m00;
+    float patch_centroid_y = m01 / m00;
     centroid_point.x = patch_centroid_x + patch_center.x;
     centroid_point.y = patch_centroid_y + patch_center.y;
     return true;
@@ -38,7 +39,8 @@ bool CalcIntensityCentroid(const cv::Mat& img,
 
 void drawIntensityCentroid(const cv::Mat& img, 
     const cv::Point2f& center, 
-    const cv::Point2f& centroid);
+    const cv::Point2f& centroid,
+    int patch_size);
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -55,30 +57,36 @@ int main(int argc, char* argv[]) {
     int width = img.cols;
     int height = img.rows;
     cv::Point2f center_point(width / 2, height / 2);
-    int patch_size = std::min(width, height);
+    int patch_size = 300;
     cv::Point2f centroid_point{};
     bool is_ok = CalcIntensityCentroid(img, center_point, patch_size, centroid_point);
     if (!is_ok) {
         std::cerr << "Calc intensity centroid failed." << std::endl;
         return -1;
     }
-    std::cerr << "Intensity centroid = " << centroid_point << std::endl;
-    drawIntensityCentroid(img, center_point, centroid_point);
+    std::cerr << "Center = " << center_point 
+        << ", Intensity centroid = " << centroid_point << std::endl;
+    
+    float theta = atan2f(centroid_point.y - center_point.y, 
+        centroid_point.x - center_point.x);
+    std::cerr << "theta = " << theta << ", " << theta / M_PI * 180 << std::endl;
+    drawIntensityCentroid(img, center_point, centroid_point, patch_size);
     return 0;
 }
 
 void drawIntensityCentroid(const cv::Mat& img, 
     const cv::Point2f& center, 
-    const cv::Point2f& centroid) {
+    const cv::Point2f& centroid,
+    int patch_size) {
     cv::Mat img_draw{};
     cv::cvtColor(img, img_draw, cv::COLOR_GRAY2BGR);
 
     // coordinates
     const int thickness = 1;
-    cv::Point2f y_axis_start_pnt(img.cols / 2, img.rows * 0.1f);
-    cv::Point2f y_axis_end_pnt(img.cols / 2, img.rows * 0.9f);
-    cv::Point2f x_axis_start_pnt(img.cols * 0.1f, img.rows / 2);
-    cv::Point2f x_axis_end_pnt(img.cols * 0.9f, img.rows / 2);
+    cv::Point2f y_axis_start_pnt(center.x, img.rows * 0.1f);
+    cv::Point2f y_axis_end_pnt(center.x, img.rows * 0.9f);
+    cv::Point2f x_axis_start_pnt(img.cols * 0.1f, center.y);
+    cv::Point2f x_axis_end_pnt(img.cols * 0.9f, center.y);
     cv::arrowedLine(img_draw, y_axis_start_pnt, y_axis_end_pnt, CV_RGB(255, 0, 0), 
         thickness, cv::LINE_AA, 0, 0.05);
     cv::arrowedLine(img_draw, x_axis_start_pnt, x_axis_end_pnt, CV_RGB(255, 0, 0),
@@ -94,7 +102,15 @@ void drawIntensityCentroid(const cv::Mat& img,
         cv::Point2f(-10, 20);
     cv::putText(img_draw, "x", x_axis_tag_pnt, 
         fontface, fontscale, cv::Scalar::all(0), thickness);
-
+    // patch range
+    int half_patch_size = patch_size / 2;
+    cv::Point2f patch_left_up_pnt = center + 
+        cv::Point2f(-half_patch_size, -half_patch_size);
+    cv::Point2f patch_right_bottom_pnt = center +
+        cv::Point2f(half_patch_size - 1, half_patch_size - 1);
+    cv::rectangle(img_draw, patch_left_up_pnt, patch_right_bottom_pnt, 
+        CV_RGB(0, 255, 0), 1, cv::LINE_AA);
+    // center, centroid
     cv::drawMarker(img_draw, center, CV_RGB(160, 32, 240), cv::MARKER_TILTED_CROSS, 10, 
         1, cv::LINE_AA);
     cv::drawMarker(img_draw, centroid, CV_RGB(255, 255, 0), cv::MARKER_TRIANGLE_UP, 10, 
