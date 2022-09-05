e884d80481e32f4ffaeac0256c5c4d18574f8d50
fseasy <readonlyfile@hotmail.com>
2022-09-05 13:56:28 +0800

    temp update

--- src/visual_odometry/pose_estimation/2d2d.cpp
+++ src/visual_odometry/pose_estimation/2d2d.cpp
@@ -3,6 +3,13 @@
 #include <opencv2/opencv.hpp>
 #include <src/visual_odometry/orb/orb.h>
 
+std::vector<std::vector<cv::Point>> _get_match_points(
+    const std::vector<std::vector<cv::KeyPoint>>& kps,
+    const std::vector<cv::DMatch>& matches);
+void eipolar_geometry(const std::vector<std::vector<cv::Point2f>>& match_points, 
+    const cv::Mat& camera_intrinsic,
+    cv::Mat& R, cv::Mat& t);
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "param error.\n"
@@ -25,5 +32,36 @@ int main(int argc, char* argv[]) {
     if (match_and_draw(descriptors, imgs, kps, matches)) {
         cv::waitKey(50000);
     }
+    auto kp2pnts_fn = [](const auto& kps, const ) {
+        std::vector<cv::Point2f> pnts{};
+        pnts.reserve(kps.size());
+        std::transform(kps.cbegin(), kps.cend(), std::back_inserter(pnts), 
+            [](const auto& kp){ return kp.pt; });
+        return pnts;
+    };
+    std::vector<std::vector<cv::Point2f>> pnts{
+        kp2pnts_fn(kps.at(0)),
+        kp2pnts_fn(kps.at(1))
+    };
+
+    cv::Mat camera_intrinsic = (cv::Mat_<double>(3, 3) 
+        << 520.9, 0, 325.1, 
+           0, 521.0, 249.7, 
+           0, 0, 1
+        );
+    cv::Mat R{};
+    cv::Mat t{};
+    epipopar_geometry(pnts, matches, camera_intrinsic, R, t);
+}
+
+std::vector<std::vector<cv::Point>> _get_match_points(
+    const std::vector<std::vector<cv::KeyPoint>>& kps,
+    const std::vector<cv::DMatch>& matches) {
+    
+}
+
+void eipolar_geometry(const std::vector<std::vector<cv::Point2f>>& match_points, 
+    const cv::Mat& camera_intrinsic,
+    cv::Mat& R, cv::Mat& t) {
     
 }
